eabe7adf6901b2a3ff5f7cc00b41976f9a7e0558
Rod Levy <rodlevy1@gmail.com>
2013-01-07 15:21:17 -0600

    Even Better RSpec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -46,7 +46,7 @@ describe "Static pages" do
     it_should_behave_like "all static pages"
   end
 
-    it "should have the right links on the layout" do
+  it "should have the right links on the layout" do
     visit root_path
     click_link "About"
     page.should have_selector 'title', text: full_title('About Us')
@@ -55,9 +55,9 @@ describe "Static pages" do
     click_link "Contact"
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
-    click_link "Sign up now!" 
-    page.should have_selector 'title', text: full_title('Sign Up')
-    click_link "sample app"
-    page.should have_selector 'title', text: full_title('Welcome to the Sample App')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+#    click_link "sample app"
+#    page.should have_selector 'title', text: full_title('Ruby on Rails Tutorial Sample App | Ruby on Rails Tutorial')
   end
 end
